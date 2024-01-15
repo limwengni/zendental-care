b997a5b3835e9f853904a2b7e995653fa8b07bda
limwengni <lim1227december@gmail.com>
2024-01-15 20:03:13 +0800

    Configure React app for deployment to GitHub Pages

---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        whitesmoke: '#f5f5f5',
+      },
+    },
+  },
+  plugins: [],
+}
+
